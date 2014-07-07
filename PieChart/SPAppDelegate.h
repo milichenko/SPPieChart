e410b8a42172c331241ff41578491288f613044b
milichenko <milichenko@maximagroup.com>
2014-07-07 17:19:36 +0300

    Initial Commit

---
+++ PieChart/SPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAppDelegate.h
+//  PieChart
+//
+//  Created by Vladimir Milichenko on 7/7/14.
+//  Copyright (c) 2014 Vladimir Milichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PieChart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PieChart
+//
+//  Created by Vladimir Milichenko on 7/7/14.
+//  Copyright (c) 2014 Vladimir Milichenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPAppDelegate class]));
+    }
+}
