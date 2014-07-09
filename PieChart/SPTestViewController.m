755ec05b19767d6049b8b646f8d882e745dc190f
milichenko <milichenko@maximagroup.com>
2014-07-09 17:27:06 +0300

    idea for future

--- PieChart/SPTestViewController.m
+++ PieChart/SPTestViewController.m
@@ -34,6 +34,14 @@
     [super viewDidLoad];
     
     self.pieChartView.pieChartBackgroundColor = self.view.backgroundColor;
+    
+    SPPieChartValue *v1 = [[SPPieChartValue alloc] initWithValue:0.1f color:[UIColor redColor]];
+    SPPieChartValue *v2 = [[SPPieChartValue alloc] initWithValue:0.2f color:[UIColor greenColor]];
+    SPPieChartValue *v3 = [[SPPieChartValue alloc] initWithValue:0.15f color:[UIColor magentaColor]];
+    SPPieChartValue *v4 = [[SPPieChartValue alloc] initWithValue:0.3f color:[UIColor yellowColor]];
+    SPPieChartValue *v5 = [[SPPieChartValue alloc] initWithValue:0.23f color:[UIColor blackColor]];
+    SPPieChartValue *v6 = [[SPPieChartValue alloc] initWithValue:1.0f color:self.view.backgroundColor];
+    self.pieChartView.pieChartValues = @[v1, v2, v3, v4, v5, v6];
 }
 
 - (void)didReceiveMemoryWarning
@@ -51,31 +59,13 @@
     SPPieChartValue *v3 = [[SPPieChartValue alloc] initWithValue:arc4random_uniform(10) / 50.0f color:[UIColor magentaColor]];
     SPPieChartValue *v4 = [[SPPieChartValue alloc] initWithValue:arc4random_uniform(10) / 50.0f color:[UIColor yellowColor]];
     SPPieChartValue *v5 = [[SPPieChartValue alloc] initWithValue:arc4random_uniform(10) / 50.0f color:[UIColor blackColor]];*/
-    
     SPPieChartValue *v1 = [[SPPieChartValue alloc] initWithValue:0.1f color:[UIColor redColor]];
     SPPieChartValue *v2 = [[SPPieChartValue alloc] initWithValue:0.2f color:[UIColor greenColor]];
     SPPieChartValue *v3 = [[SPPieChartValue alloc] initWithValue:0.15f color:[UIColor magentaColor]];
     SPPieChartValue *v4 = [[SPPieChartValue alloc] initWithValue:0.3f color:[UIColor yellowColor]];
     SPPieChartValue *v5 = [[SPPieChartValue alloc] initWithValue:0.23f color:[UIColor blackColor]];
-    
-    self.pieChartView.pieChartValues = @[v1, v2, v3, v4, v5];
-    
-    /*NSMutableArray *randomPieValues = [NSMutableArray array];
-	int count = 1 + rand() % 10;
-	
-    for (int i = 0; i < count; i++)
-    {
-        NSInteger aRedValue = arc4random()%255;
-        NSInteger aGreenValue = arc4random()%255;
-        NSInteger aBlueValue = arc4random()%255;
-        
-        UIColor *randColor = [UIColor colorWithRed:aRedValue/255.0f green:aGreenValue/255.0f blue:aBlueValue/255.0f alpha:1.0f];
-        SPPieChartValue *v = [[SPPieChartValue alloc] initWithValue:rand() % 10 color:randColor];
-		//[randomNumbers addObject:[NSNumber numberWithInt:rand() % 100]];
-        [randomPieValues addObject:v];
-	}
-	
-	self.pieChartView.pieChartValues = randomPieValues;*/
+    SPPieChartValue *v6 = [[SPPieChartValue alloc] initWithValue:0.0f color:self.view.backgroundColor];
+    self.pieChartView.pieChartValues = @[v1, v2, v3, v4, v5, v6];
 }
 
 @end
